b5ece89017bac6b0a82e5ff50765d4a32f968176
skc <kc7851@gmail.com>
2019-10-28 17:44:02 +0900

    7.3.2 외부 profile 설정

--- src/main/java/com/skc/webservice/Application.java
+++ src/main/java/com/skc/webservice/Application.java
@@ -1,15 +1,31 @@
 package com.skc.webservice;
 
+import io.micrometer.core.instrument.util.JsonUtils;
+import org.apache.coyote.AbstractProcessorLight;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.io.File;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
+    public static String APPLICATION_LOCATION = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        if (System.getProperty("os.name").contains("Windows")) {
+            APPLICATION_LOCATION = "spring.config.location="
+                    + "classpath:application.yml,"
+                    + System.getProperty("user.home") + File.separator + "real-application.yml";
+        }
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATION)
+                .run(args);
     }
 
 }
--- src/main/java/com/skc/webservice/web/WebRestController.java
+++ src/main/java/com/skc/webservice/web/WebRestController.java
@@ -1,19 +1,16 @@
 package com.skc.webservice.web;
 
-import com.skc.webservice.domain.posts.Posts;
 import com.skc.webservice.domain.posts.PostsRepository;
-import com.skc.webservice.dto.posts.PostsMainResponseDto;
 import com.skc.webservice.dto.posts.PostsSaveRequestDto;
 import com.skc.webservice.service.PostsService;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
@@ -21,7 +18,16 @@ public class WebRestController {
 
     private PostsService postsService;
 
+    private Environment env;
+
     private PostsRepository postsRepository;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
 
     @GetMapping("/hello")
     public String hello() {
